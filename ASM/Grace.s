ed04d2c15a7d894833323e740e88984ebe47b193
Joey PLEVY <jplevy@e1r7p14.42.fr>
2018-08-21 19:04:24 +0200

    ok

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -2,8 +2,8 @@
 DEFAULT REL
 section .data
 
-msg:    db      ";outside comment%1$cDEFAULT REL%1$csection .data%1$c%1$cmsg:    db      %2$c%3$s%2$c, 0%1$cfile:    db      %2$cGrace_kid.asm%2$c, 0%1$cmode:    db      %2$cw%2$c, 0%1$cnl:     db      0x0a%1$cquote:  db      0x22%1$c%1$csection .text%1$cglobal start%1$cglobal _main%1$cextern _fprintf%1$cextern _fopen%1$c%1$cstart:%1$c    call _main%1$c    ret%1$c%1$c_main:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, [file]%1$c    lea rsi, [mode]%1$c    mov rax, 0%1$c    call _fopen%1$c    mov rdi, rax %1$c    lea rsi, [msg]%1$c    mov rdx, [nl]%1$c    mov rcx, [quote]%1$c    lea r8, [msg]%1$c    mov rax, 0%1$c    call _fprintf%1$c    leave%1$c    ret%1$c", 0
-file:    db      "Grace_kid.asm", 0
+msg:    db      ";outside comment%1$cDEFAULT REL%1$csection .data%1$c%1$cmsg:    db      %2$c%3$s%2$c, 0%1$cfile:    db      %2$cGrace_kid.s%2$c, 0%1$cmode:    db      %2$cw%2$c, 0%1$cnl:     db      0x0a%1$cquote:  db      0x22%1$c%1$csection .text%1$cglobal start%1$cglobal _main%1$cextern _fprintf%1$cextern _fopen%1$c%1$cstart:%1$c    call _main%1$c    ret%1$c%1$c_main:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, [file]%1$c    lea rsi, [mode]%1$c    mov rax, 0%1$c    call _fopen%1$c    mov rdi, rax %1$c    lea rsi, [msg]%1$c    mov rdx, [nl]%1$c    mov rcx, [quote]%1$c    lea r8, [msg]%1$c    mov rax, 0%1$c    call _fprintf%1$c    leave%1$c    ret%1$c", 0
+file:    db      "Grace_kid.s", 0
 mode:    db      "w", 0
 nl:     db      0x0a
 quote:  db      0x22
